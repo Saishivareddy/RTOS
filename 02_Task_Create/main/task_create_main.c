2f69b41b8af2042fef8921c8d6d8299c279a18bb
Saishivareddy <saishivareddy75@gmail.com>
2022-08-10 16:09:34 +0530

    Task Creation

---
+++ 02_Task_Create/main/task_create_main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+TaskHandle_t serial_task_handle;
+
+void serial_task(void *data)
+{
+    printf("In the Created Task\n");
+}
+
+void app_main()
+{
+    BaseType_t res;
+    res = xTaskCreate(serial_task, "SERIALTASK", 2048, NULL, 5, &serial_task_handle);
+    if(res == pdPASS)
+    {
+        printf("Task Created Successfully\n");
+    }
+}
